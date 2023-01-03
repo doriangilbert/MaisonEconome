d77f46291d88c119556b1ab934efe3bcaa4d5e3d
doriangilbert <29594904+doriangilbert@users.noreply.github.com>
2023-01-03 16:54:42 +0100

    Correction du lien avec la base de données pour les pages de base

--- modele/connexion.php
+++ modele/connexion.php
@@ -4,7 +4,8 @@ try{
     // Connection using PDO
     //$db = new PDO("mysql:host=localhost;dbname=maisonconnectee", "root", "");
     // Connection using mysqli
-    $bdd = mysqli_connect("127.0.0.1", "root", "","maisonconnectee");
+    //$bdd = mysqli_connect("127.0.0.1", "root", "","maisonconnectee");
+    $bdd = mysqli_connect("127.0.0.1", "root", "","maisoneconome");
 }
 catch(Exception $e)
 {
