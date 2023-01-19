cf201ff961b15c48f0b8d6b877f48ad8367e8d39
Sylver-Wolf <djfalcon37510@gmail.com>
2023-01-19 11:59:50 +0100

    début ajout page d'informations du profil utilisateur

---
+++ maison_econome_site/MaisonEconome/vues/info_profil.php
@@ -0,0 +1,46 @@
+<?php 
+if(!isset($_SESSION['id']) && !isset($_SESSION['userTag'])){
+?>
+
+<h2 class="titre">Inscription</h2>
+
+<div class="login-box">
+    <form action="./models/update_profile_form.php" method="post" class="form">
+
+        <?php if (isset($error)) {
+            include('models/error.php');
+        } ?>
+
+        <label>Nom :</label>
+        <input type="text" name="name" placeholder="Nom" value=<?php $_SESSION['userSurname'] ?> required><br>
+
+        <label>Prénom :</label>
+        <input type="text" name="surname" placeholder="Prénom" value=<?php $_SESSION['userFirstname'] ?> required><br>
+
+        <label>Adresse E-mail :</label>
+        <input type="text" name="userEmail" placeholder="Adresse E-mail" autocomplete="off" value=<?php $_SESSION['userEmail'] ?> required><br>
+
+        <label>Date de naissance :</label>
+        <input type="date" name="birthDate" placeholder="Date de naissance" value=<?php $_SESSION['userBirth'] ?> required><br>
+
+        <label>Genre :</label>
+        <select name="genre" required>
+            <option value="male">Homme</option>
+            <option value="female">Femme</option>
+        </select>
+
+        <label>Numéro de téléphone :</label>
+        <input type="text" name="userPhone" placeholder="Numéro de téléphone" autocomplete="off" pattern="\d{10,10}" required><br>
+
+        <button type="submit">S'inscrire</button>
+    </form>
+</div>
+<div class="div-block">
+    <a class="a-button" href="index.php?cible=main&fonction=register">Se connecter</a>
+</div>
+
+<?php
+}else{
+    header("Location: index.php?cible=main&fonction=home");
+}
+?>
